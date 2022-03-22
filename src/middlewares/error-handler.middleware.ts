98dd7f6c9ec8ef782cdb499823491fa7c47b0297
JNCJcoder <jorgenettocjunior@gmail.com>
2022-03-22 15:31:58 -0300

    fix ErrorHandler

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,16 +1,16 @@
 import type { Context } from "https://deno.land/x/oak@v10.4.0/mod.ts";
-import { Status, isHttpError } from "https://deno.land/x/oak@v10.4.0/mod.ts";
+import { isHttpError } from "https://deno.land/x/oak@v10.4.0/mod.ts";
 
-async function errorHandler(context: Context, next: () => any): Promise<void> {
+async function errorHandler(context: Context, next: () => Promise<unknown>): Promise<void> {
     try {
         await next();
     } catch (error) {
-        if (!isHttpError(error)) {
+        if (isHttpError(error)) {
             context.response.status = error.status;
-            context.response.body = error.message;
+            context.response.body = { message: error.message };
         }
         else {
-            console.log(`Error: ${Status.InternalServerError} | Unexpected Error`);
+            console.log(`Error: ${error.status} | ${error.message}`);
             context.response.status = error.status;
             context.response.body = error.message;
         }
