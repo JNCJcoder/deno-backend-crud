bc91844225998edaaa30ed4f79441972b7b9f890
JNCJcoder <jorgenettocjunior@gmail.com>
2022-03-22 14:09:58 -0300

    Fix ClientController types

--- src/controllers/ClientController.ts
+++ src/controllers/ClientController.ts
@@ -1,37 +1,37 @@
 import db from "../database/connect.ts";
 import type { ClientInterface } from "../models/Client.ts";
-import type { Context, RouterContext } from "https://deno.land/x/oak/mod.ts";
-import { Status } from "https://deno.land/x/oak/mod.ts";
+import type { Context } from "https://deno.land/x/oak/mod.ts";
+import { helpers, Status } from "https://deno.land/x/oak/mod.ts";
 
 class ClienteController {
-  public async index({ response }: Context) {
+  public async index(context: Context) {
     try {
-      const users = JSON.parse(await db.getData());
+      const users = await db.getData();
 
-      response.body = users;
+      context.response.body = users;
     } catch (err) {
       console.log(err);
-      response.status = Status.InternalServerError;
-      response.body = { msg: err };
+      context.response.status = Status.InternalServerError;
+      context.response.body = { msg: err };
     }
   }
 
-  public async create({ request, response }: Context) {
-    if (!request.hasBody) {
-      response.status = Status.BadRequest;
-      response.body = { msg: "Invalid data" };
+  public async create(context: Context) {
+    if (!context.request.hasBody) {
+      context.response.status = Status.BadRequest;
+      context.response.body = { msg: "Invalid data" };
       return;
     }
 
-    const { name }: { name: string } = await request.body().value;
+    const { name }: { name: string } = await context.request.body().value;
 
     if (!name) {
-      response.status = Status.UnprocessableEntity;
-      response.body = { msg: "Name is required." };
+      context.response.status = Status.UnprocessableEntity;
+      context.response.body = { msg: "Name is required." };
       return;
     }
 
-    const ClientList: Array<ClientInterface> = JSON.parse(await db.getData());
+    const ClientList: Array<ClientInterface> = await db.getData();
 
     ClientList.push({
       id: ClientList.length + 1,
@@ -39,44 +39,45 @@ class ClienteController {
     });
 
     await db.updateData(ClientList);
-    response.status = Status.Created;
-    response.body = { msg: "Client added successfully" };
+    context.response.status = Status.Created;
+    context.response.body = { msg: "Client added successfully" };
   }
 
-  public async read({ params, response }: RouterContext<string>) {
-    const clientID = Number(params.id);
+  public async read(context: Context) {
+    const clientID = Number(helpers.getQuery(context, { mergeParams: true }));
+    
 
     if (!clientID) {
-      response.status = Status.BadRequest;
-      response.body = { msg: "Invalid Client ID" };
+      context.response.status = Status.BadRequest;
+      context.response.body = { msg: "Invalid Client ID" };
       return;
     }
 
-    const ClientList: Array<ClientInterface> = JSON.parse(await db.getData());
+    const ClientList: Array<ClientInterface> = await db.getData();
     const clientFound = ClientList.find(({ id }) => id == clientID);
 
     if (!clientFound) {
-      response.status = Status.NotFound;
-      response.body = { msg: `Client ${clientID} does not exist` };
+      context.response.status = Status.NotFound;
+      context.response.body = { msg: `Client ${clientID} does not exist` };
       return;
     }
 
-    response.status = Status.OK;
-    response.body = clientFound;
+    context.response.status = Status.OK;
+    context.response.body = clientFound;
   }
 
-  public async update({ params, request, response }: RouterContext<string>) {
-    const clientID = Number(params.id);
+  public async update(context: Context) {
+    const clientID = Number(helpers.getQuery(context, { mergeParams: true }));
 
     if (!clientID) {
-      response.status = Status.BadRequest;
-      response.body = { msg: "Invalid Client ID" };
+      context.response.status = Status.BadRequest;
+      context.response.body = { msg: "Invalid Client ID" };
       return;
     }
     
-    const { name }: { name: string } = await request.body().value;
+    const { name }: { name: string } = await context.request.body().value;
 
-    const ClientList: Array<ClientInterface> = JSON.parse(await db.getData());
+    const ClientList: Array<ClientInterface> = await db.getData();
 
     const ClientListUpdated = ClientList.filter(client => {
       if (client.id == clientID) {
@@ -87,32 +88,32 @@ class ClienteController {
     });
     await db.updateData(ClientListUpdated);
 
-    response.status = Status.OK;
-    response.body = { msg: `Client ${clientID} updated successfully!` };
+    context.response.status = Status.OK;
+    context.response.body = { msg: `Client ${clientID} updated successfully!` };
   }
 
-  public async delete({ params, response }: RouterContext<string>) {
-    const clientID = Number(params.id);
+  public async delete(context: Context) {
+    const clientID = Number(helpers.getQuery(context, { mergeParams: true }));
 
     if (!clientID) {
-      response.status = Status.BadRequest;
-      response.body = { msg: "Invalid Client ID." };
+      context.response.status = Status.BadRequest;
+      context.response.body = { msg: "Invalid Client ID." };
       return;
     }
 
-    const ClientList: Array<ClientInterface> = JSON.parse(await db.getData());
+    const ClientList: Array<ClientInterface> = await db.getData();
     const ClientListUpdated = ClientList.filter(client => client.id != clientID);
 
     if (ClientList.length === ClientListUpdated.length) {
-      response.status = Status.NotFound;
-      response.body = { msg: `Client ${clientID} does not exist.` };
+      context.response.status = Status.NotFound;
+      context.response.body = { msg: `Client ${clientID} does not exist.` };
       return;
     }
 
     await db.updateData(ClientListUpdated);
 
-    response.status = Status.OK;
-    response.body = { msg: `Client ${clientID} deleted successfully!` };
+    context.response.status = Status.OK;
+    context.response.body = { msg: `Client ${clientID} deleted successfully!` };
   }
 }
 
